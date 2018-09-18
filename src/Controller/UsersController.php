a3f9258ef68df6d840fd14b75ef9ebed44607f6e
tatsuo-iriyama <tatsuo.iriyama@yourmystar.jp>
2018-09-18 23:43:56 +0900

    新規登録処理追加

--- src/Controller/UsersController.php
+++ src/Controller/UsersController.php
@@ -11,6 +11,76 @@ class UsersController extends AppController
 {
     public function index()
     {
+        $user = $this->Users->newEntity();
+
+        // postデータか判定
+        if ($this->request->is('post')) {
+            // Entity作成
+            $validateUser = $this->Users->newEntity($this->request->data, [
+                'validate' => 'register'
+            ]);
+
+            if ($validateUser->errors()) {
+                // バリデーションに引っかかった場合
+                $this->Flash->error('入力内容に不備があります');
+                $this->set('user', $validateUser);
+                return $this->render($this->request->action, 'default');
+            }
+
+            // 入力内容をセッションに格納
+            $this->request->session()->write('registerUser', $validateUser);
+        }
+
+        $this->render($this->request->action, 'default');
+    }
+
+    public function confirm()
+    {
+        // 入力内容をセッションから取得
+        $user = $this->request->session()->read('registerUser');
+
+        if (!$user) {
+            // セッションから情報を取得できなかった場合
+            return throw new BadRequestException('セッションの情報を取得できませんでした');
+        }
+
+        $this->set(compact('user'));
+        $this->render($this->request->action, 'default');
+    }
+
+    public function save()
+    {
+        // 入力内容をセッションから取得
+        $user = $this->request->session->consume('registerUser');
+
+        if (!$user) {
+            // セッションから情報を取得できなかった場合
+            return throw new BadRequestException('セッションの情報を取得できませんでした');
+        }
+
+        // 入力したパスワードをハッシュ化して渡す
+        $user->password_hash = $user['password'];
+
+        if (!$this->Users->save($user)) {
+            // DBに格納できなかった場合
+            return $this->Flash->error('入力内容を保存できませんでした');
+        }
+
+        // 登録内容でそのままログイン
+        $this->Auth->setUser($user->toArray());
+        $this->redirect('Users/complete');
+    }
+
+    public function complete()
+    {
+        // ログインユーザー情報の取得
+        $authUser = $this->Auth->user();
+
+        if (empty($authUser)) {
+            // ログインユーザーの取得ができなかった場合
+            return throw new BadRequestException('登録情報の取得ができませんでした');
+        }
+
         $this->render($this->request->action, 'default');
     }
 }
