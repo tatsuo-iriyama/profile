53aaa1353dbf1274f8980f67ef959041ae39a3a5
tatsuo-iriyama <tatsuo.iriyama@yourmystar.jp>
2018-11-03 22:29:55 +0900

    DB設定変更

--- config/app.default.php
+++ config/app.default.php
@@ -215,9 +215,9 @@ return [
              * the following line and set the port accordingly
              */
             //'port' => 'nonstandard_port_number',
-            'username' => 'my_app',
-            'password' => 'secret',
-            'database' => 'my_app',
+            'username' => 'root',
+            'password' => '',
+            'database' => 'sample',
             'encoding' => 'utf8',
             'timezone' => 'UTC',
             'cacheMetadata' => true,
